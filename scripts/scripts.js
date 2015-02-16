40cbe1eb8c1a8ab51c807080034871a76e0af9c9
bkalb <billykalb@gmail.com>
2015-02-16 14:22:57 -0600

    Insufferable Rabbits! Round 1!
    I’m not wild about the background color transition, but everything else
    is pretty neat.

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('#swap-rap').click(function() {
+        $('#content').addClass('swap');
+    });
+    $('#swap-bio').click(function() {
+        $('#content').removeClass('swap');
+    });
+});
